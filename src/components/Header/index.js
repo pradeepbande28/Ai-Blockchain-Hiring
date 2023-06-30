3ffd3092e12f79b8a0a6ef8403c1387633095671
Pradeep Bande <pradeep@agomic.com>
2023-06-30 21:26:48 +0530

    added sign text functionality and Sign Text link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,80 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 // reactstrap components
 import { Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { ethers } from 'ethers';
 
 import './index.scss';
+
 function HomePage() {
 
   const [isConnected, setIsConnected] = useState(false);
+  const [signer, setSigner] = useState(null);
+  const handleAccountsChangedRef = useRef(null);
 
   useEffect(() => {
-    // Check if Metamask is installed
+
     if (typeof window.ethereum == 'undefined') {
       alert("No Metamask!")
     }
+
+    const checkConnection = async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      if (accounts.length > 0) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        setIsConnected(true);
+        setSigner(signer);
+      }
+    };
+
+    const handleAccountsChanged = (accounts) => {
+      if (accounts.length > 0) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        setIsConnected(true);
+        setSigner(signer);
+      } else {
+        setIsConnected(false);
+        setSigner(null);
+      }
+    };
+
+    handleAccountsChangedRef.current = handleAccountsChanged;
+    window.ethereum.on('accountsChanged', handleAccountsChangedRef.current);
+
+    checkConnection();
+
+    return () => {
+      handleAccountsChangedRef.current = null;
+    };
   }, []);
 
   const connectWallet = async () => {
     if (!isConnected) {
       try {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        if (accounts && accounts[0]) {
-          setIsConnected(true)
-        }
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        setIsConnected(true);
+        setSigner(signer);
       } catch (error) {
         console.log(error);
       }
+    }
+  };
+
+  const signText = async () => {
+    if (!signer) {
+      alert('Please connect to MetaMask first!');
+      return;
+    }
+    const textToSign = 'Hello, Ai-Blockchain!';
+    try {
+      const signature = await signer.signMessage(textToSign);
+      console.log("Signature --- ", signature)
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -49,6 +98,12 @@ function HomePage() {
         </Link>
       </Col>
       <Col xs="4" className="logo" >
+        {
+          isConnected &&
+          <a className="margin-12" onClick={signText}>
+            Sign Text
+          </a>
+        }
         <a className="margin-12" onClick={connectWallet}>
           {isConnected ? 'Connected' : 'Connect Wallet'}
         </a>
